4dadb13511a51551f7eb1e4068a539ea3b2e29d6
bassembuto <132224648+bassembuto@users.noreply.github.com>
2023-11-04 20:06:39 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/****************** Typing Animation ****************/
+
+var typed = new Typed('.typing', {
+    strings: ['','Web Developer', 'Web Designer', 'Sponsored Ads', 'YouTuber'],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+})
